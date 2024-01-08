156abecf8c5ae66ca42fda36cdcd7d3094ec123a
Juliamanda <julia.isotalo@edu.sasky.fi>
2024-01-08 12:03:42 +0200

    Lisää Header-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
-import styles from './App.module.scss'
+import Header from '../Header'
 import Item from '../Item'
+import styles from './App.module.scss'
 
 function App() {
 
   return (
     <>
       <div className={styles.app}>
+        <Header />
         <Item />
         <Item />
         <Item />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import styles from './Header.module.scss'
+
+function Header() {
+
+  return (
+    <div className={styles.header}>
+      Treenipäiväkirja
+    </div>
+  )
+
+}
+
+export default Header
