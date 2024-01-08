346d9faa9e6ec5a2bc6c5a166c27a02a36d9df8a
Juliamanda <julia.isotalo@edu.sasky.fi>
2024-01-08 13:43:04 +0200

    Lsää linkityksen Menu-komponenttiin

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,14 +2,15 @@ import styles from './Menu.module.scss'
 import { MdOutlineSettings } from "react-icons/md";
 import { MdShowChart } from "react-icons/md";
 import { MdHome } from "react-icons/md";
+import { NavLink } from 'react-router-dom'
 
 function Menu() {
 
   return (
     <div className={styles.menu}>
-      <div><MdHome /></div>
-      <div><MdShowChart /></div>
-      <div><MdOutlineSettings /></div>
+      <div><NavLink to=""><MdHome /></NavLink></div>
+      <div><NavLink to="/stats"><MdShowChart /></NavLink></div>
+      <div><NavLink to="/settings"><MdOutlineSettings /></NavLink></div>
     </div>
   )
 
