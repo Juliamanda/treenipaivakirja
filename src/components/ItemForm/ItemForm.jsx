3f1f6bf65e3d6e13380b7d77749b86aefe8f495d
Juliamanda <julia.isotalo@edu.sasky.fi>
2024-01-09 16:31:56 +0200

    Lisää lisäyslomakkeen peruutuksen

--- src/components/ItemForm/ItemForm.jsx
+++ src/components/ItemForm/ItemForm.jsx
@@ -1,8 +1,11 @@
 import Button from '../../shared/buttons'
 import styles from './ItemForm.module.scss'
 import useForm from '../../shared/useform/useform'
+import { useNavigate } from 'react-router-dom'
 
 function ItemForm(props) {
+  const navigate = useNavigate()
+
   const submit = () => {
     console.log(values)
     alert("SUBMIT")
@@ -18,7 +21,7 @@ function ItemForm(props) {
   const {values, handleChange, handleSubmit } = useForm(submit, initialState, false)
   
   const handleCancel = () => {
-    alert('CANCEL') 
+    navigate('/')
   }
 
   return (
