9b898cad9f34a8e82954cbc13f32bec26d305e94
Juliamanda <julia.isotalo@edu.sasky.fi>
2024-01-09 12:46:26 +0200

    Lisää AddItem- ja ItemForm-komponentit sekä reitityksen

---
+++ src/components/AddItem/AddItem.jsx
@@ -0,0 +1,14 @@
+import ItemForm from '../ItemForm/ItemForm'
+import styles from './AddItem.module.scss'
+
+function AddItem(props) {
+
+  return (
+    <div>
+      <h2><ItemForm /></h2>
+    </div> 
+  )
+
+}
+
+export default AddItem
