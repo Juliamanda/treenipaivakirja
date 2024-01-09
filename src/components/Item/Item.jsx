3caa26da8a5cc7569306508908ed5726994a2c9e
Juliamanda <julia.isotalo@edu.sasky.fi>
2024-01-09 12:21:41 +0200

    Lsää päivämäärämuotoilun Item-komponentille

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,13 +3,15 @@ import { MdNavigateNext } from 'react-icons/md'
 
 
 function Item({data, ...props}) {
+  const locale = "fi-FI"
+  const date = new Date(data.date).toLocaleDateString(locale)
 
     return (
         <div className={styles.item}>
           <div className={styles.item_data}>
             <div className={styles.item_type}>{data.type}</div>
             <div className={styles.item_set}>{data.set}</div>
-            <div className={styles.item_date}>{data.date}</div>
+            <div className={styles.item_date}>{date}</div>
             <div className={styles.item_repeat}>{data.repeat} x {data.weight} kg</div>
             <div className={styles.item_time}>{data.time}</div>
             <div className={styles.item_sum}>? kg</div>
