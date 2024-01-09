f55d1bcfc7b44e49c3dcf3f4a661f1c0cd4728bf
Juliamanda <julia.isotalo@edu.sasky.fi>
2024-01-09 12:06:39 +0200

    Lsää sovelluksen testidatan

---
+++ src/components/App/testdata.js
@@ -0,0 +1,53 @@
+const testdata = [
+    {
+      id:          "1",
+      type:        "Maastaveto",
+      set:         "2",
+      date:        "2023-03-20",
+      repeat:      "12",
+      wight:       "60",
+    },
+    {
+      id:          "2",
+      type:        "Jalkaprässi",
+      set:         "3",
+      date:        "2023-04-20",
+      repeat:      "10",
+      wight:       "70",
+    },
+    {
+      id:          "3",
+      type:        "Kulmasoutu",
+      set:         "3",
+      date:        "2023-03-19",
+      repeat:      "15",
+      wight:       "20",
+    },  
+    {
+      id:          "4",
+      type:        "Ylätalja",
+      set:         "4",
+      date:        "2023-03-19",
+      repeat:      "13",
+      wight:       "25",
+    },
+    {
+      id:          "5",
+      type:        "Reiden ojennus",
+      set:         "4",
+      date:        "2023-03-20",
+      repeat:      "8",
+      wight:       "65",
+    },
+    {
+      id:          "6",
+      type:        "Penkkipunnerrus",
+      set:         "4",
+      date:        "2023-04-20",
+      repeat:      "10",
+      wight:       "50",
+    } 
+  ]
+  
+  export default testdata
+  
