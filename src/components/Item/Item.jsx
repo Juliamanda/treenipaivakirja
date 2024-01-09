80822ce60b6941de34743ede5eb00abd1b9b66d5
Juliamanda <julia.isotalo@edu.sasky.fi>
2024-01-09 12:06:25 +0200

    Muokkaa Item komponenttia

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -8,9 +8,9 @@ function Item() {
         <div className={styles.item}>
           <div className={styles.item_data}>
             <div className={styles.item_type}>Maastaveto</div>
-            <div className={styles.item_weight}>60 kg</div>
+            <div className={styles.item_set}>3</div>
             <div className={styles.item_date}>20.3.2023</div>
-            <div className={styles.item_repeat}>3 x 4</div>
+            <div className={styles.item_repeat}>12 x 40 kg</div>
           </div>
           <div className={styles.item_edit}>
             <MdNavigateNext />
