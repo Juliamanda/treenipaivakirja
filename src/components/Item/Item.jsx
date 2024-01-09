28e9e0d1eae93c5601df920a85ec410b6955d4eb
Juliamanda <julia.isotalo@edu.sasky.fi>
2024-01-09 12:34:33 +0200

    Lsää kokonaiskilomäärän laskennan

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -6,6 +6,11 @@ function Item({data, ...props}) {
   const locale = "fi-FI"
   const date = new Date(data.date).toLocaleDateString(locale)
 
+  let sum 
+  if (data.repeat && data.weight && data.set) {
+    sum = data.set * data.repeat * data.weight;
+    
+  }
     return (
         <div className={styles.item}>
           <div className={styles.item_data}>
@@ -14,7 +19,7 @@ function Item({data, ...props}) {
             <div className={styles.item_date}>{date}</div>
             <div className={styles.item_repeat}>{data.repeat} x {data.weight} kg</div>
             <div className={styles.item_time}>{data.time}</div>
-            <div className={styles.item_sum}>? kg</div>
+            <div className={styles.item_sum}>{sum + " " + "kg"}</div>
           </div>
           <div className={styles.item_edit}>
             <MdNavigateNext />
