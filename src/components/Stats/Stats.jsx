bfa37edb66c3f135e56461dd790829e64549136a
Juliamanda <julia.isotalo@edu.sasky.fi>
2024-01-10 12:49:32 +0200

    Lisää aikajanan piirtävän viivakaavion

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,10 +1,37 @@
+import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 import styles from './Stats.module.scss'
 
-function Stats() {
+function Stats(props) {
+
+  const locale = "fi-FI"
+  
+  const linedata = props.data.map(
+    (item) => ({
+      date: new Date(item.date).getTime(),
+      weight: item.weight
+    })
+  )
 
   return (
     <div className={styles.stats}>
-      <h2>Stats</h2>
+            <h2>Tilastot</h2>
+            <h3>Kulut aikajanalla</h3>
+      <ResponsiveContainer height={350}>
+        <LineChart data={linedata}>
+          <Line dataKey='weight' />
+          <XAxis type='number'  
+                 dataKey='date' 
+                 domain={['dataMin','dataMax']}
+                 tickFormatter={
+                   value => new Date(value).toLocaleDateString(locale)
+                 } />
+          <YAxis />
+          <Tooltip labelFormatter={ 
+                     value => new Date(value).toLocaleDateString(locale)
+                   } />
+        </LineChart>
+      </ResponsiveContainer>
+
     </div>
   )
 }
